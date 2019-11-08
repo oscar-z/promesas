1e9b13fd637ba6add92e84e875af12efb479a8f6
Oscar <oscar.ims93@gmail.com>
2019-11-07 21:02:15 -0600

    creamos reques.post

--- index.js
+++ index.js
@@ -9,3 +9,28 @@ const app = express();
 //** Config bodyParser */
 app.use(bodyParser.urlencoded({ extended: true}));
 app.use(bodyParser.json());
+
+//** Post */
+function crear () {
+    let author = {
+        name: "Oscar",
+        last_name: "Martinez",
+        nacionalidad: "MX",
+        biography: ":) <3",
+        gender: "M",
+        age: 26,
+        is_alive: true
+    }           
+    request.post({url:'https://goodreads-devf-aaron.herokuapp.com/api/v1/authors/', form: author}, (request, response, body) => {
+        let bodyToJson = JSON.parse(body);
+     console.log(bodyToJson);
+    })
+}
+
+// request.get('https://goodreads-devf-aaron.herokuapp.com/api/v1/authors', (err,response,body) =>{
+//     let bodyToJson = JSON.parse(body);
+//     console.log(bodyToJson());
+// })
+
+crear();
+
